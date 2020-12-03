d2ba2d1d6dea9cb6908dda58b228fa7d522988a1
PinkLaura <34890444+PinkLaura@users.noreply.github.com>
2020-12-03 18:04:40 +0100

    Altro con la navbar

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $(function() {
+    $('.navbar-toggler').click(function() {
+      $('#ham-icon').toggleClass('hidden');
+      $('#ham-closed-icon').toggleClass('hidden');  
+    });
+  });
+
+});
+
